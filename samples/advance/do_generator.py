30f1b94ec5d4be1cb88b6a87cfd74fc1b15e710e
xienova <xiechunhui1990@126.com>
2017-09-10 14:29:56 +0800

    add the yanghui triangle

--- samples/advance/do_generator.py
+++ samples/advance/do_generator.py
@@ -6,6 +6,8 @@ print(s)
 for x in s:
     print(x)
 
+    
+# Fibonacci数列的定义与调用
 def fib(max):
     n, a, b = 0, 0, 1
     while n < max:
@@ -29,3 +31,18 @@ while 1:
         print('Generator return value:', e.value)
         break
 
+        
+# 杨辉三角
+def triangles():
+    L = [1,0]       # 此列表的末尾有 0 元素
+    while True:
+        yield L[:-1]        # 0 元素不让其显示，只显示前面的元素
+        L = [1] + [L[x] + L[x-1] for x in range(1, len(L))] + [0]
+
+n = 0
+for t in triangles():
+    print(t)
+    n = n+1
+    if n == 10:
+        break
+        
