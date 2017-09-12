2a72526071f216aff5d621b93ea5ee46b39f46f2
xienova <xiechunhui1990@126.com>
2017-09-13 07:34:09 +0800

    ANS1

--- samples/functional/do_map.py
+++ samples/functional/do_map.py
@@ -5,3 +5,12 @@ def f(x):
     return x * x
 
 print(list(map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+
+
+# 将用户输入的不规范英文名字，变为 首字母大写，其他小写的 规范名字。
+def normalize(name):
+    return name.capitalize()
+
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
