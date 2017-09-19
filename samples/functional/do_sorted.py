2d67ffd3c85d5f666c3d09cbb36873755c70b22c
xienova <xiechunhui1990@126.com>
2017-09-19 18:35:48 +0800

    Update do_sorted.py

--- samples/functional/do_sorted.py
+++ samples/functional/do_sorted.py
@@ -10,6 +10,10 @@ print(sorted(L, key=str.lower))
 
 students = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 
+
 print(sorted(students, key=itemgetter(0)))
 print(sorted(students, key=lambda t: t[1]))
 print(sorted(students, key=itemgetter(1), reverse=True))
+
+
+# 说明：operator.itemgetter() 返回一个函数，函数作用到一个对象上才能获得值。  
